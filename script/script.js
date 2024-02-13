1ae4b3c4962af3a13be19d99b65f44ecc04884de
Salah Ahmed <riddenariel@gmail.com>
2024-02-14 01:58:12 +0200

    Bug fix and add sound
    Fixed bugs, added sound on click, updated GUI

--- script/script.js
+++ script/script.js
@@ -1,3 +1,7 @@
+let audio = new Audio(
+	"https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/click.mp3"
+);
+
 function generateNumber(min, max) {
 	return Math.floor(Math.random() * (max - min + 1) + min);
 }
@@ -17,16 +21,22 @@ function getComputerChoice() {
 
 const rockBtn = document.querySelector("#rock");
 rockBtn.addEventListener("click", () => {
+	audio.load();
+	audio.play();
 	playGame("rock");
 });
 
 const paperBtn = document.querySelector("#paper");
 paperBtn.addEventListener("click", () => {
+	audio.load();
+	audio.play();
 	playGame("paper");
 });
 
 const scissorsBtn = document.querySelector("#scissors");
 scissorsBtn.addEventListener("click", () => {
+	audio.load();
+	audio.play();
 	playGame("scissors");
 });
 
@@ -42,129 +52,60 @@ let playerScore = 0;
 function playGame(playerSelection) {
 	let player = playerSelection;
 	let computer = getComputerChoice();
-
+	if (computerScore === 0 && playerScore === 0) {
+		whoWins.innerText = `First to five will win`;
+	}
 	if (player == "rock" && computer == "scissors") {
 		playerScore++;
 		round.innerText = `You won this round!`;
 		pScore.innerText = `Player score is ${playerScore} `;
 		cScore.innerText = `Computer score is ${computerScore}`;
-		if (playerScore == 5) {
-			whoWins.innerText = `Player won the game! First to five! Resetting game`;
-			computerScore = 0;
-			playerScore = 0;
-		} else if (computerScore == 5) {
-			whoWins.innerText = `Computer won the game! First to five! Resetting game`;
-			computerScore = 0;
-			playerScore = 0;
-		}
 	} else if (player == "rock" && computer == "paper") {
 		computerScore++;
 		round.innerText = `Computer won this round!`;
 		pScore.innerText = `Player score is ${playerScore} `;
 		cScore.innerText = `Computer score is ${computerScore}`;
-		if (playerScore == 5) {
-			whoWins.innerText = `Player won the game! First to five! Resetting game`;
-			computerScore = 0;
-			playerScore = 0;
-		} else if (computerScore == 5) {
-			whoWins.innerText = `Computer won the game! First to five! Resetting game`;
-			computerScore = 0;
-			playerScore = 0;
-		}
 	} else if (player == "rock" && computer == "rock") {
 		round.innerText = `Tie!`;
 		pScore.innerText = `Player score is ${playerScore} `;
 		cScore.innerText = `Computer score is ${computerScore}`;
-		if (playerScore == 5) {
-			whoWins.innerText = `Player won the game! First to five! Resetting game`;
-			computerScore = 0;
-			playerScore = 0;
-		} else if (computerScore == 5) {
-			whoWins.innerText = `Computer won the game! First to five! Resetting game`;
-			computerScore = 0;
-			playerScore = 0;
-		}
 	} else if (player == "paper" && computer == "scissors") {
 		computerScore++;
 		round.innerText = `Computer won this round!`;
 		pScore.innerText = `Player score is ${playerScore} `;
 		cScore.innerText = `Computer score is ${computerScore}`;
-		if (playerScore == 5) {
-			whoWins.innerText = `Player won the game! First to five! Resetting game`;
-			computerScore = 0;
-			playerScore = 0;
-		} else if (computerScore == 5) {
-			whoWins.innerText = `Computer won the game! First to five! Resetting game`;
-			computerScore = 0;
-			playerScore = 0;
-		}
 	} else if (player == "paper" && computer == "rock") {
 		playerScore++;
 		round.innerText = `You won this round!`;
 		pScore.innerText = `Player score is ${playerScore} `;
 		cScore.innerText = `Computer score is ${computerScore}`;
-		if (playerScore == 5) {
-			whoWins.innerText = `Player won the game! First to five! Resetting game`;
-			computerScore = 0;
-			playerScore = 0;
-		} else if (computerScore == 5) {
-			whoWins.innerText = `Computer won the game! First to five! Resetting game`;
-			computerScore = 0;
-			playerScore = 0;
-		}
 	} else if (player == "paper" && computer == "paper") {
 		round.innerText = `Tie!`;
 		pScore.innerText = `Player score is ${playerScore} `;
 		cScore.innerText = `Computer score is ${computerScore}`;
-		if (playerScore == 5) {
-			whoWins.innerText = `Player won the game! First to five! Resetting game`;
-			computerScore = 0;
-			playerScore = 0;
-		} else if (computerScore == 5) {
-			whoWins.innerText = `Computer won the game! First to five! Resetting game`;
-			computerScore = 0;
-			playerScore = 0;
-		}
 	} else if (player == "scissors" && computer == "paper") {
 		playerScore++;
 		round.innerText = `You won this round!`;
 		pScore.innerText = `Player score is ${playerScore} `;
 		cScore.innerText = `Computer score is ${computerScore}`;
-		if (playerScore == 5) {
-			whoWins.innerText = `Player won the game! First to five! Resetting game`;
-			computerScore = 0;
-			playerScore = 0;
-		} else if (computerScore == 5) {
-			whoWins.innerText = `Computer won the game! First to five! Resetting game`;
-			computerScore = 0;
-			playerScore = 0;
-		}
 	} else if (player == "scissors" && computer == "rock") {
 		computerScore++;
 		round.innerText = `Computer won this round!`;
 		pScore.innerText = `Player score is ${playerScore} `;
 		cScore.innerText = `Computer score is ${computerScore}`;
-		if (playerScore == 5) {
-			whoWins.innerText = `Player won the game! First to five! Resetting game`;
-			computerScore = 0;
-			playerScore = 0;
-		} else if (computerScore == 5) {
-			whoWins.innerText = `Computer won the game! First to five! Resetting game`;
-			computerScore = 0;
-			playerScore = 0;
-		}
 	} else if (player == "scissors" && computer == "scissors") {
 		round.innerText = `Tie!`;
 		pScore.innerText = `Player score is ${playerScore} `;
 		cScore.innerText = `Computer score is ${computerScore}`;
-		if (playerScore == 5) {
-			whoWins.innerText = `Player won the game! First to five! Resetting game`;
-			computerScore = 0;
-			playerScore = 0;
-		} else if (computerScore == 5) {
-			whoWins.innerText = `Computer won the game! First to five! Resetting game`;
-			computerScore = 0;
-			playerScore = 0;
-		}
+	}
+
+	if (playerScore == 5) {
+		whoWins.innerText = `You won the game! Click a button to start a new game.`;
+		computerScore = 0;
+		playerScore = 0;
+	} else if (computerScore == 5) {
+		whoWins.innerText = `Computer won the game! Click a button to start a new game`;
+		computerScore = 0;
+		playerScore = 0;
 	}
 }
